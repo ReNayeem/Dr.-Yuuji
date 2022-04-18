4279f61eb9fe854c95817a1e57bad1d75aa3dd4c
ReNayeem <renayeem@gmail.com>
2022-04-18 15:23:23 +0600

    about me done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,8 +3,10 @@ import './About.css'
 
 const About = () => {
     return (
-        <div className='about-page'>
-            <h2>This is About us</h2>
+        <div className='about-page text-center container d-flex flex-column'>
+            <h2>Assalamualaikum. This is <span className='text-success'>Nayeem</span>. I'm studying B.Sc. in CSE at Daffodil Internation University. My goal is to become a professional WEB developer. To fulfill my goal I continuously working/studying/practicing hard where I can't even sleep 5 hours a day. So please pray for me to succeed.
+            </h2>
+            <h3 className='mt-5'>Thank you.🙂</h3>
         </div>
     );
 };
